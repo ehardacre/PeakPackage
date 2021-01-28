3c4d871722ed14b4b9b762ffe64edab96c5c88c7
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-01-28 13:32:24 -0800

    changed image extension for calendar

--- Sources/PeakPackage/Extensions/Image+Custom.swift
+++ Sources/PeakPackage/Extensions/Image+Custom.swift
@@ -4,11 +4,6 @@ import SwiftUI
 
 extension Image {
 
-    public static var uTurnLeft: Image = {
-        guard let image = UIImage(systemName:"arrow.uturn.left") else {
-            fatalError("Error: `ElegantCalendar.xcassets` doesn't exist. Refer to the `README.md` installation on how to resolve this.")
-        }
-        return Image(uiImage: image)
-    }()
+    public static var uTurnLeft: Image = Image(systemName: "arrow.uturn.left")
 
 }
