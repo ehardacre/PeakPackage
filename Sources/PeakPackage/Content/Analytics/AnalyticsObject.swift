f2dc9d033c2589c45d50b91a37e4aca80b915bca
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-03-09 11:51:47 -0800

    analytics

--- Sources/PeakPackage/Content/Analytics/AnalyticsObject.swift
+++ Sources/PeakPackage/Content/Analytics/AnalyticsObject.swift
@@ -306,7 +306,7 @@ struct GA_TotalsForAllResults_Page : Codable {
     
     //NHANCE AND PEAK
     var gasessions : String
-    var gatotalEvents : String
+    var gatotalEvents : String//
     
     //WOOCOMMERCE
     var gatransactionRevenue : String?
