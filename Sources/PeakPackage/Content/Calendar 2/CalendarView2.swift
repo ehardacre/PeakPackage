bef033b668cb02d1d0054eaa9a10cd2a8c296a8a
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-04-30 08:56:25 -0700

    new spacing

--- Sources/PeakPackage/Content/Calendar 2/CalendarView2.swift
+++ Sources/PeakPackage/Content/Calendar 2/CalendarView2.swift
@@ -24,19 +24,22 @@ struct CalendarView2: View {
     @State var selectionMan = CalendarSelectionManager()
     
     var body: some View {
-        GeometryReader{ geo in
-            TabView(selection: $selectedMonth) {
-                MonthView2(selectionMan: selectionMan, month: .last)
-                    .tag(0)
-                MonthView2(selectionMan: selectionMan, month: .current)
-                    .tag(1)
-                MonthView2(selectionMan: selectionMan, month: .next)
-                    .tag(2)
+        VStack{
+            Spacer()
+            GeometryReader{ geo in
+                TabView(selection: $selectedMonth) {
+                    MonthView2(selectionMan: selectionMan, month: .last)
+                        .tag(0)
+                    MonthView2(selectionMan: selectionMan, month: .current)
+                        .tag(1)
+                    MonthView2(selectionMan: selectionMan, month: .next)
+                        .tag(2)
+                }
+                .frame(width: geo.size.width)
+                .tabViewStyle(PageTabViewStyle())
             }
-            .frame(width: geo.size.width)
-            .tabViewStyle(PageTabViewStyle())
+            Spacer()
         }
-        .padding(.bottom, 50)
     }
 }
 
