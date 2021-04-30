4f231c82624088fee7af0d65dc050f4c2f85727e
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-04-30 10:06:07 -0700

    idk more attempts at good spacing

--- Sources/PeakPackage/Content/Calendar 2/MonthView2.swift
+++ Sources/PeakPackage/Content/Calendar 2/MonthView2.swift
@@ -64,25 +64,25 @@ struct MonthView2: View{
     var body: some View {
         GeometryReader{ geo in
             VStack{
-                VStack{
-                    Text(month.getName())
-                        .CardTitle()
-                    LazyVGrid(columns: columns){
-                        ForEach(columnHeaders, id: \.self) { header in
-                            Text(header)
-                                .font(.footnote)
-                        }
-                        ForEach(month.getDays(selectionMan), id: \.id) { dateView in
-                            dateView
-                        }
+                Text(month.getName())
+                    .CardTitle()
+                LazyVGrid(columns: columns){
+                    ForEach(columnHeaders, id: \.self) { header in
+                        Text(header)
+                            .font(.footnote)
+                    }
+                    ForEach(month.getDays(selectionMan), id: \.id) { dateView in
+                        dateView
+                    }
+                    ForEach(0..<7, id: \.self) { _ in
+                        Text("").frame(height: 40) ///blank rows for spacing fix
                     }
                 }
-                .padding(20)
-                .frame(width: geo.size.width)
-                .background(Color.darkAccent.opacity(0.1))
-                .cornerRadius(20)
-                Spacer(minLength: 50)
             }
+            .padding(20)
+            .frame(width: geo.size.width)
+            .background(Color.darkAccent.opacity(0.1))
+            .cornerRadius(20)
         }
         .padding(20)
     }
