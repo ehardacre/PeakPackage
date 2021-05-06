986613d78be3c2040ccc170e215b2c97f628e2a9
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-05-06 10:05:42 -0700

    testing incremented time picker

--- Sources/PeakPackage/Content/Tasks2/New Task/AutoForm.swift
+++ Sources/PeakPackage/Content/Tasks2/New Task/AutoForm.swift
@@ -260,6 +260,51 @@ struct DateInputCardView : View {
     }
 }
 
+struct MyDatePicker: UIViewRepresentable {
+
+    @Binding var selection: Date
+    let minuteInterval: Int = 30
+    let displayedComponents: DatePickerComponents
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<MyDatePicker>) -> UIDatePicker {
+        let picker = UIDatePicker()
+        // listen to changes coming from the date picker, and use them to update the state variable
+        picker.addTarget(context.coordinator, action: #selector(Coordinator.dateChanged), for: .valueChanged)
+        return picker
+    }
+
+    func updateUIView(_ picker: UIDatePicker, context: UIViewRepresentableContext<MyDatePicker>) {
+        picker.minuteInterval = minuteInterval
+        picker.date = selection
+
+        switch displayedComponents {
+        case .hourAndMinute:
+            picker.datePickerMode = .time
+        case .date:
+            picker.datePickerMode = .date
+        case [.hourAndMinute, .date]:
+            picker.datePickerMode = .dateAndTime
+        default:
+            break
+        }
+    }
+
+    class Coordinator {
+        let datePicker: MyDatePicker
+        init(_ datePicker: MyDatePicker) {
+            self.datePicker = datePicker
+        }
+
+        @objc func dateChanged(_ sender: UIDatePicker) {
+            datePicker.selection = sender.date
+        }
+    }
+}
+
 struct TimeInputCardView : View {
     
     var id : UUID
@@ -277,14 +322,7 @@ struct TimeInputCardView : View {
 //                .Caption()
 //            DatePicker("", selection: $input, displayedComponents: [.date,.hourAndMinute])
             
-            Picker("Time:", selection: $timeIndex){
-                ForEach(hourOpts, id: \.self){
-                    time in
-                    Text(time)
-                        .CardTitle()
-                }
-            }
-            .pickerStyle(SegmentedPickerStyle())
+            MyDatePicker(selection: $input, displayedComponents: .hourAndMinute)
             
         }
         .BasicContentCard()
