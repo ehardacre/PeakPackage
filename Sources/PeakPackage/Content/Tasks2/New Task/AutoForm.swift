ddb98b18f64d4d8e07afd69b0f22a4e60c312adf
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-05-10 11:30:35 -0700

    style

--- Sources/PeakPackage/Content/Tasks2/New Task/AutoForm.swift
+++ Sources/PeakPackage/Content/Tasks2/New Task/AutoForm.swift
@@ -275,7 +275,6 @@ struct TimeInputCardView : View {
     var body : some View {
         HStack{
             DatePicker("", selection: $selectedDay, displayedComponents: .date)
-            Spacer()
             Button(action: {
                 pickingTime = true
             }, label: {
