6fd1fdd492376f0556788306b2cc09d9fdcc78b7
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-05-14 10:35:13 -0700

    bug fix I think

--- Sources/PeakPackage/Extensions/Calender+Dates.swift
+++ Sources/PeakPackage/Extensions/Calender+Dates.swift
@@ -61,7 +61,7 @@ extension Date {
         let timezone = TimeZone.current
         var seconds = -TimeInterval(timezone.secondsFromGMT(for: self))
         if timezone.isDaylightSavingTime(for: self) {
-            seconds = seconds - 3600
+            seconds = seconds + 3600
         }
         return Date(timeInterval: seconds, since: self)
     }
