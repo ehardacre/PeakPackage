4c8a7b21a0b8598b05c429905d4e0d99e268663f
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-06-04 07:48:25 -0700

    more print statements

--- Sources/PeakPackage/DatabaseDelegate.swift
+++ Sources/PeakPackage/DatabaseDelegate.swift
@@ -504,7 +504,6 @@ struct DatabaseDelegate {
     static func objectFrom(data: Data, type: returnType) throws -> Any{
         
         //MARK: objectFrom
-        printr(data)
         var rex : Any?
         //switch for the return type to determine which type to cast as
         switch type {
@@ -537,6 +536,8 @@ struct DatabaseDelegate {
         default:
             rex = String.init(data: data, encoding: .ascii)!
         }
+        
+        printr(String.init(data: data, encoding: .ascii)!)
         //rex cannot be nil from the data base
         guard rex != nil else { throw DataError.nilResponse }
         return rex!
