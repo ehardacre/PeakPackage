02f92c0d06971f1905e81d6a422009781f232d26
Ethan Hardacre <hardacre.ethan@gmail.com>
2021-07-30 06:40:47 -0700

    added support for pie chart but didn't implement it

---
+++ Sources/PeakPackage/Content/Accessory Views/PieChart.swift
@@ -0,0 +1,120 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ethan Hardacre  on 7/30/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct PieSliceView: View {
+    var pieSliceData: PieSliceData
+    
+    var midRadians: Double {
+        return Double.pi / 2.0 - (pieSliceData.startAngle + pieSliceData.endAngle).radians / 2.0
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Path { path in
+                    let width: CGFloat = min(geometry.size.width, geometry.size.height)
+                    let height = width
+                    
+                    let center = CGPoint(x: width * 0.5, y: height * 0.5)
+                    
+                    path.move(to: center)
+                    
+                    path.addArc(
+                        center: center,
+                        radius: width * 0.5,
+                        startAngle: Angle(degrees: -90.0) + pieSliceData.startAngle,
+                        endAngle: Angle(degrees: -90.0) + pieSliceData.endAngle,
+                        clockwise: false)
+                    
+                }
+                .fill(pieSliceData.color)
+                
+                Text(pieSliceData.text)
+                    .position(
+                        x: geometry.size.width * 0.5 * CGFloat(1.0 + 0.78 * cos(self.midRadians)),
+                        y: geometry.size.height * 0.5 * CGFloat(1.0 - 0.78 * sin(self.midRadians))
+                    )
+                    .foregroundColor(Color.white)
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+struct PieSliceData {
+    var startAngle: Angle
+        var endAngle: Angle
+        var text: String
+        var color: Color
+}
+
+struct PieChartView: View {
+    public let values: [Double]
+    public var colors: [Color]
+//        [Color(.RGBColorSpace, red: 77, green: 157, blue: 224, opacity: 1.0),
+//                                  Color(Color.RGBColorSpace, red: 225, green: 85, blue: 84, opacity: 1.0),
+//                                  Color(Color.RGBColorSpace, red: 225, green: 188, blue: 41, opacity: 1.0),
+//                                  Color(Color.RGBColorSpace, red: 59, green: 178, blue: 115, opacity: 1.0),
+//                                  Color(Color.RGBColorSpace, red: 119, green: 104, blue: 174, opacity: 1.0)]
+    public let names: [String]
+    
+    public var backgroundColor: Color
+    public var innerRadiusFraction: CGFloat = 0.6
+    
+    var slices: [PieSliceData] {
+        let sum = values.reduce(0, +)
+        var endDeg: Double = 0
+        var tempSlices: [PieSliceData] = []
+//        var tempValues: [Double] = []
+//        var tempNames:
+//
+//        for (i, value) in values.sorted().reversed().enumerated() {
+//            if i <= colors.count {
+//                tempValues.append(value)
+//            }else{
+//                tempValues[colors.count] = tempValues[colors.count] + value
+//            }
+//        }
+        
+        for (i, value) in values.enumerated() {
+            let degrees: Double = value * 360 / sum
+            tempSlices.append(PieSliceData(startAngle: Angle(degrees: endDeg), endAngle: Angle(degrees: endDeg + degrees), text: String(format: "%.0f%%", value * 100 / sum), color: self.colors[i]))
+            endDeg += degrees
+        }
+        return tempSlices
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack{
+                ZStack{
+                    ForEach(0..<self.values.count){ i in
+                        PieSliceView(pieSliceData: self.slices[i])
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.width)
+                    
+                    Circle()
+                        .fill(self.backgroundColor)
+                        .frame(width: geometry.size.width * innerRadiusFraction, height: geometry.size.width * innerRadiusFraction)
+                    
+                    VStack {
+                        Text("Total")
+                            .font(.title)
+                            .foregroundColor(Color.gray)
+                        Text(String(values.reduce(0, +)))
+                            .font(.title)
+                    }
+                }
+            }
+            .background(self.backgroundColor)
+            .foregroundColor(Color.white)
+        }
+    }
+}
